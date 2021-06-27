71940b7c6f7c2fc015938ff38a608e9ec5701dae
Daniel Garcia <daniel.garcia7913@gmail.com>
2021-06-27 00:39:14 -0400

    i'll make it better x3

--- server/index.js
+++ server/index.js
@@ -32,6 +32,7 @@ io.on('connection', function(socket){
     })
 })
 
-server.listen(6677, function() {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, function() {
     console.log('Server on http://localhost:6677');
 });
