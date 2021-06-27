b218b37c8795565a274024677a9bfc323547d326
Gabriel A. Rodriguez Ubiera <grodriguezubiera@gmail.com>
2021-06-27 18:31:53 -0400

    Better code

--- server/index.js
+++ server/index.js
@@ -1,38 +1,47 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const server = require('http').Server(app)
 
-let server = require('http').Server(app)
+// Settings
+app.set('port', process.env.PORT || 3000);
 
-let io = require('socket.io')(server,{
-    cors: { // Permite el acceso de orígenes mixtos (CORS)
-        origin: '*'
-    },
-});
-
+// Middlewares
 app.use(express.static('client'));
+app.use(cors());
 
-let messages = [{
+// Routes
+app.get('/hola-mundo', (req, res) => {
+    res.status(200).json({message: 'Hola mundo!'});
+})
+
+async function init(){
+    await server.listen(app.get('port'), ()  => {
+        console.log(`Server on http://localhost:${app.get('port')}`);
+    });
+}
+
+init();
+
+const messages = [{
     id: 1,
     text: 'Bienvenido al chat privado de Socket.io mmg',
     nickname: 'Bot - kanedaboi_'
 }]
 
-app.get('/hola-mundo', function(req, res) {
-    res.status(200).send('Hola mundo!');
-})
+const io = require('socket.io')(server,{
+    cors: { // Permite el acceso de orígenes mixtos (CORS)
+        origin: '*'
+    },
+});
 
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
     console.log(`El cliente con IP ${socket.handshake.address} se ha conectado`);
 
     socket.emit('messages', messages);
 
-    socket.on('add-message', function(data) {
+    socket.on('add-message', (data) => {
         messages.push(data);
         io.sockets.emit('messages', messages);
     })
 })
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, function() {
-    console.log(`Server on http://localhost:${PORT}`);
-});
